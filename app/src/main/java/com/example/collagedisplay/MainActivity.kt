7cd69cbd836d4f28bfd20e7fdb4d96db02543e4e
PearMyst <email>
2022-04-20 20:56:43 -0500

    Finished from videos

--- app/src/main/java/com/example/collagedisplay/MainActivity.kt
+++ app/src/main/java/com/example/collagedisplay/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.collagedisplay
 
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -9,9 +10,14 @@ import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ProgressBar
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import java.io.File
@@ -26,9 +32,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mainImageButton: ImageButton
     private lateinit var mainView: View
+    private lateinit var uploadImageFab: FloatingActionButton
+    private lateinit var uploadProgressBar: ProgressBar
 
     private var newImagePath: String? = null
     private var visibleImagePath: String? = null
+    private var imageFileName: String? = null
+    private var imageUri: Uri? = null
+
+    private val storage = Firebase.storage
 
     private val NEW_IMAGE_PATH_KEY = "new image path key"
     private val VISIBLE_IMAGE_PATH_KEY = "visible image path key"
@@ -48,9 +60,39 @@ class MainActivity : AppCompatActivity() {
         visibleImagePath = savedInstanceState?.getString(VISIBLE_IMAGE_PATH_KEY)
 
         mainImageButton = findViewById(R.id.main_image_button)
+        uploadProgressBar = findViewById(R.id.uploadProgressBar)
+        uploadImageFab = findViewById(R.id.uploadImageButton)
+        mainView = findViewById(R.id.content_layout)
 
         mainImageButton.setOnClickListener{
             takePicture()
+        }
+
+        uploadImageFab.setOnClickListener {
+            uploadImage()
+        }
+    }
+
+    private fun uploadImage() {
+        if (imageUri != null && imageFileName != null) {
+
+            uploadProgressBar.visibility = View.VISIBLE
+
+            val imageStorageRootReference = storage.reference
+            val imageCollectionReference = imageStorageRootReference.child("images")
+            val imageFileReference = imageCollectionReference.child(imageFileName!!)
+
+            imageFileReference.putFile(imageUri!!).addOnCompleteListener {
+                Snackbar.make(mainView, "Image uploaded!", Snackbar.LENGTH_SHORT).show()
+                uploadProgressBar.visibility = View.GONE
+            }
+                .addOnFailureListener { error ->
+                    Snackbar.make(mainView, "Error uploading image", Snackbar.LENGTH_LONG).show()
+                    Log.e(TAG, "error uploading image $imageFileName", error)
+                    uploadProgressBar.visibility = View.GONE
+                }
+        } else {
+            Snackbar.make(mainView, "Take a picture first!", Snackbar.LENGTH_LONG).show()
         }
     }
 
@@ -67,7 +109,7 @@ class MainActivity : AppCompatActivity() {
         if (imageFile != null) {
             // This is a refrence to the image file.
             newImagePath = imageFilePath
-            val imageUri = FileProvider.getUriForFile(this,
+            imageUri = FileProvider.getUriForFile(this,
                 "com.example.collagedisplay.fileprovider",
                 imageFile) // Creates a URI for the new image file that was created.
 
@@ -82,7 +124,7 @@ class MainActivity : AppCompatActivity() {
     private fun createImageFile(): Pair<File?, String?>{
         try {
             val dateTime = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) // Used to create a unique file name
-            val imageFileName = "COLLAGE_$dateTime" // Adds our unique timeStamp to the photo name.
+            imageFileName = "COLLAGE_$dateTime" // Adds our unique timeStamp to the photo name.
             val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES) // Checks the default image storage
             val file = File.createTempFile(imageFileName, ".jpg", storageDir)
             val filePath = file.absolutePath
